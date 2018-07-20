1c92c5aafb69fb55e44830f2b321a473ff7f235c
christos dousmanopoulos <christos_dousmanopoulos@yahoo.gr>
2018-07-20 11:56:48 +0300

    added main catalan_triangle.py

---
+++ catalan_triangle.py
@@ -0,0 +1,12 @@
+size = 6
+
+tbl = []
+for i in range(size):
+        temp = [1]
+        for j in range(size-1):
+                temp.append(0)
+        tbl.append(temp) 
+
+for row in range(1,size):
+        for col in range(1,row + 1):
+                tbl[row][col] = tbl[row-1][col] + tbl[row][col-1]
